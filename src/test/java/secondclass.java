2c2e4fbb620b50b09529b5b67c33523fb05a8e30
MNizami <administrator@Administrators-MacBook-Pro.local>
2020-03-11 09:21:57 -0400

    secondclass

--- src/test/java/secondclass.java
+++ src/test/java/secondclass.java
@@ -2,7 +2,7 @@ public class secondclass {
 
     public static void main(String[] args) {
         System.out.println("cybertek");
-        System.out.println("it worked out");
+        System.out.println("it worked out ");
 System.out.println("hope it will work");
         
     }
